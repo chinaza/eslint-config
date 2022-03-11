ecd10527684f618d8063451ce15c51a08ad60e98
chinaza <hi@chinaza.dev>
2022-03-11 03:06:12 +0000

    feat: add support for prettier

---
+++ prettier.js
@@ -0,0 +1,23 @@
+module.exports = {
+  ignorePath: '.prettierignore',
+
+  jsxBracketSameLine: false,
+
+  printWidth: 80,
+
+  proseWrap: 'preserve',
+
+  requireConfig: false,
+
+  semi: true,
+
+  singleQuote: true,
+
+  stylelintIntegration: false,
+
+  tabWidth: 2,
+
+  trailingComma: 'none',
+
+  useTabs: false
+};
