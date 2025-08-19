dcf75c432881d7f877df8f0e3cecf05e084dccbc
chinaza <hi@chinaza.dev>
2025-08-19 01:14:24 +0100

    remove no unsafe call

--- express.eslint.config.js
+++ express.eslint.config.js
@@ -121,6 +121,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
 
       // Code quality for TypeScript
       "prefer-arrow-callback": "warn",
